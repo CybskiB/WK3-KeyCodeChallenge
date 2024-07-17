64bbffe9f0a25133f9b7657129fd55dda10cb9a5
Liam <liamj9898@icloud.com>
2024-07-17 11:30:57 +0100

    Fix the big font

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ let key = document.getElementById("key")
 
 document.addEventListener("keypress", () => {
     topText.textContent = event.keyCode
-    topText.style.fontSize = "23vh"
-    topText.style.top = "-50vh"
     keyValue.textContent = event.key
     keyCode.textContent = event.code
     charCode.textContent = event.charCode
